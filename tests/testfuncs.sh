36c4ec625fa25bce1862458405c2c0239d446428
suyashmahar <suyash12mah.uec2016@iitr.ac.in>
2018-01-09 19:09:53 +0530

    Add basic tests

---
+++ tests/testfuncs.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+# File: testfuncs.sh
+# ##
+# Lists all the tests for pine
+
+echo "Starting tests..."
+echo "Creating a directory for performing tests and switching to it..."
+mkdir testingspace
+
+echo "Starting pine..."
+`../../pine` || exit 1
+
+echo "Testing configs for c and c++"
+`../../pine c c++` || exit 1
+
+echo "Listing all configurations..."
+`../../pine --list` || exit 1
+
+echo "Listing all configurations..."
+`../../pine --list` || exit 1
+
+echo "Printing help text..."
+`../../pine --help` || exit 
+
+echo "Printing version..."
+`../../pine --version` || exit 1
